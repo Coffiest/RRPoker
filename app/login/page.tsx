58976546961c5832653777fe57627e01e902f68c
naoyuki <hagiwara.naoyuki327@mail.kyutech.jp>
2026-04-09 12:22:48 +0900

    feat: minor update after rrLeaderboard fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -227,7 +227,7 @@ export default function LoginPage() {
 
   <div className="pt-[50px] text-center">
     
-               <p className="mt-2 text-[10px] text-gray-500">ver 1.3.0</p>
+               <p className="mt-2 text-[10px] text-gray-500">ver 1.3.1.1</p>
                 <p className="mt-2 text-[10px] text-gray-500">RRPoker by Runner Runner</p>
                 <p className="mt-2 text-[10px] text-gray-500">製作者 : Turn dead man</p>
         </div>
