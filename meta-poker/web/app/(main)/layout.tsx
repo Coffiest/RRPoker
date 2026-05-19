36210c42fe6eb8edc3bcfdc9b15e25e38dedf2fe
naoyuki <hagiwara.naoyuki327@mail.kyutech.jp>
2026-05-19 11:44:41 +0900

    feat: ver1.6.2

--- meta-poker/web/app/(main)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export const dynamic = 'force-dynamic';
-
-import { MainLayoutClient } from './MainLayoutClient';
-
-export default function MainLayout({ children }: { children: React.ReactNode }) {
-  return <MainLayoutClient>{children}</MainLayoutClient>;
-}
